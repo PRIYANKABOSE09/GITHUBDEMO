44bc24b8489bcdebbf1b26e9a2e5cd45c86664f7
PRIYANKABOSE09 <93118182+PRIYANKABOSE09@users.noreply.github.com>
2021-11-16 12:03:01 +0530

    Update mycode2.py

--- mycode2.py
+++ mycode2.py
@@ -41,3 +41,14 @@ for i in range(rows, 0, -1):
     for j in range(0, i):
         print(num, end=' ')
     print("\r")
+
+
+
+
+rows = 5
+# reverse loop
+for i in range(rows, 0, -1):
+    num = i
+    for j in range(0, i):
+        print(num, end=' ')
+    print("\r")
